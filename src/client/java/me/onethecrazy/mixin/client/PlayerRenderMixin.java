8058da65cde569058ed13e730befcc860839ddf9
1TheCrazy <3thecrazy@gmail.com>
2025-07-10 11:37:23 +0200

    Add Render Mixin

--- src/client/java/me/onethecrazy/mixin/client/PlayerRenderMixin.java
+++ src/client/java/me/onethecrazy/mixin/client/PlayerRenderMixin.java
@@ -1,5 +1,7 @@
 package me.onethecrazy.mixin.client;
 
+import me.onethecrazy.AllTheSkins;
+import me.onethecrazy.util.Vertex;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
@@ -8,15 +10,15 @@ import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.RotationAxis;
-import org.joml.Matrix3f;
 import org.joml.Matrix4f;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 @Mixin(LivingEntityRenderer.class)
 public class PlayerRenderMixin {
@@ -28,7 +30,7 @@ public class PlayerRenderMixin {
 
             matrixStack.push();
             matrixStack.translate(0, 0, 0);
-            matrixStack.scale(1f, 1f, 1f);
+            matrixStack.scale(0.05f, 0.05f, 0.05f);
 
             // Apply player Yaw
             float rot = playerState.relativeHeadYaw + state.bodyYaw;
@@ -38,17 +40,18 @@ public class PlayerRenderMixin {
             MatrixStack.Entry entry = matrixStack.peek();
             Matrix4f matrix = entry.getPositionMatrix();
 
-            RenderLayer layer = RenderLayer.getDebugQuads();
+            RenderLayer layer = RenderLayer.getEntityCutoutNoCull(Identifier.of(AllTheSkins.MOD_ID, "textures/white_pixel.png"));
             VertexConsumer buffer = vertexConsumerProvider.getBuffer(layer);
 
             float r = 1f, g = 1f, b = 1f, a = 1f;
-            float min = -0.5f;
-            float max = 0.5f;
 
-            buffer.vertex(matrix, min, min, max).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(entry, 0, 0, 1);
-            buffer.vertex(matrix, max, min, max).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(entry, 0, 0, 1);
-            buffer.vertex(matrix, max, max, max).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(entry, 0, 0, 1);
-            buffer.vertex(matrix, min, max, max).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(entry, 0, 0, 1);
+            if(AllTheSkins.HEAD_MODEL.isPresent()){
+                List<Vertex> verticies = AllTheSkins.HEAD_MODEL.get();
+
+                for(Vertex v : verticies){
+                    buffer.vertex(matrix, v.position.x, v.position.y, v.position.z).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(entry, v.normals.x, v.normals.y, v.normals.z);
+                }
+            }
 
             matrixStack.pop();
 
--- src/main/java/me/onethecrazy/AllTheSkins.java
+++ src/main/java/me/onethecrazy/AllTheSkins.java
@@ -1,9 +1,14 @@
 package me.onethecrazy;
 
+import me.onethecrazy.util.OBJParser;
+import me.onethecrazy.util.Vertex;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Optional;
 
 public class AllTheSkins implements ModInitializer {
 	public static final String MOD_ID = "all-the-skins";
@@ -12,6 +17,8 @@ public class AllTheSkins implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final Optional<List<Vertex>> HEAD_MODEL = OBJParser.Parse("C:\\Users\\User\\Downloads\\random-male-human-head-base-mesh\\source\\random head\\random head.obj");
 
 	@Override
 	public void onInitialize() {
