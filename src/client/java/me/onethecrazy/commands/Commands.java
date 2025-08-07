e71b6616431070232bacf25b68a297dfa9224dc5
1TheCrazy <3thecrazy@gmail.com>
2025-08-07 20:44:26 +0200

    Add /skin toggle command

--- src/client/java/me/onethecrazy/commands/Commands.java
+++ src/client/java/me/onethecrazy/commands/Commands.java
@@ -3,6 +3,7 @@ package me.onethecrazy.commands;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import me.onethecrazy.AllTheSkinsClient;
 import me.onethecrazy.ClientFileUtil;
 import me.onethecrazy.SkinManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -19,16 +20,22 @@ public class Commands {
     public static void initializeCommands(){
         SKINS_COMMAND = ClientCommandManager.literal("skin")
                 .then(ClientCommandManager.literal("set")
-                            .executes(context -> waypointsCommandHandler(context, AllTheSkinsCommandType.SET)));
+                        .executes(context -> waypointsCommandHandler(context, AllTheSkinsCommandType.SET)))
+                .then(ClientCommandManager.literal("toggle")
+                        .executes(context -> waypointsCommandHandler(context, AllTheSkinsCommandType.TOGGLE)));
                 //.then(ClientCommandManager.literal("copy")
-                //        .then(ClientCommandManager.argument("name", StringArgumentType.string()).executes(context -> waypointsCommandHandler(context, AllTheSkinsCommandType.COPY)))
+                //       .then(ClientCommandManager.argument("name", StringArgumentType.string()).executes(context -> waypointsCommandHandler(context, AllTheSkinsCommandType.COPY)))
                 //);
     }
 
     private static int waypointsCommandHandler(CommandContext<FabricClientCommandSource> ctx, AllTheSkinsCommandType cmdType){
         switch(cmdType){
-            case AllTheSkinsCommandType.SET -> {
+            case SET -> {
                 SkinManager.pickClientSkin();
+                return 1;
+            }
+            case TOGGLE -> {
+                AllTheSkinsClient.options().isEnabled = !AllTheSkinsClient.options().isEnabled;
                 return 1;
             }
             case COPY -> {
@@ -42,6 +49,7 @@ public class Commands {
 
     private enum AllTheSkinsCommandType {
         SET,
+        TOGGLE,
         COPY
     }
 }
